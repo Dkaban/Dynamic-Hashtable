6384e4524b5f95b96c216d5ec04d361144d29980
Dustin <dustinkaban@gmail.com>
2021-04-14 12:30:42 -0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -2,9 +2,7 @@
 //  Book.java
 //
 //  AUTHOR: DUSTIN KABAN
-//  ID: T00663749
 //  DATE: APRIL 14th, 2021
-//  COURSE INFO: COMP 2231 ASSIGNMENT 5, QUESTION 3
 //
 //  This is a book class that allows us to create a hashtable of books with multiple pieces of information.
 //  Used by DynamicHashTable.java
